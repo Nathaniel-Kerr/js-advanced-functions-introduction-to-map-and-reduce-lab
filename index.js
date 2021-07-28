c2b36b0c1fed9e8a0daf1a4437bb932c680503a9
Nathaniel <NathanielKerr10@gmail.com>
2021-07-28 13:35:58 -0700

    commit tree eb8350a0b95570f45469b7bd2fde256a95572d80

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+function mapToNegativize(array) {
+    return array.map( x => x * -1);
+}
+
+function  mapToNoChange(array) {
+    return array.map(x => x);
+}
+
+function  mapToDouble(array) {
+    return array.map(x => x * 2);
+}
+
+function  mapToSquare(array) {
+    return array.map(x => x * x);
+}
+
+function reduceToTotal(array, start = 0) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return array.reduce(reducer, start);
+}
+
+function reduceToAllTrue(array) {
+    const reducer = function(accumulator, currentValue){
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return array.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(array) {
+    const reducer = function(accumulator, currentValue){
+        if(accumulator === true){
+            return true;
+        } else {
+            return !!currentValue;
+        }
+    }
+    return array.reduce(reducer)
+}
